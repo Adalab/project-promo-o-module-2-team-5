eb955e4b54abce32093be20f402be8be9f7bb846
Claudia <claudia.berrocalgomez@gmail.com>
2021-12-10 14:05:08 +0100

    add java script design

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('>> Ready :)');
+//hide box
+
+const titleBoxDesign = document.querySelector('.design');
+const coloursDesign = document.querySelector('.paletteandcolours');
+const arrowDown = document.querySelector('.fa-chevron-down');
+const arrowUp = document.querySelector('.fa-chevron-up');
+
+function closeDesign() {
+  coloursDesign.classList.toggle('collapsed');
+  arrowDown.classList.toggle('collapsed');
+  arrowUp.classList.toggle('collapsed');
+}
+
+function ClickCloseMenu() {
+  closeDesign();
+}
+
+titleBoxDesign.addEventListener('click', ClickCloseMenu);
+
+//change colours
+
+const palette1 = document.querySelector('.card__preview--title');
+const colours2 = document.querySelector('#colours2');
+
+function changePalette() {
+  palette1.classList.add('title2');
+}
+
+colours2.addEventListener('click', changePalette);
