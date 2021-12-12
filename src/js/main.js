9fca6653a0f1ee7f3fb6fdd5340415fe252b37e2
Norma <normarivasmadrid@gmail.com>
2021-12-12 09:10:57 +0100

    add regular expresions

--- src/js/main.js
+++ src/js/main.js
@@ -109,49 +109,46 @@ function handleFill(e) {
 //evento de escuchar el formulario entero(Todos los inputs)
 form.addEventListener('keyup', handleFill);
 
-
 //----------------------------------------------------------------------//
 //----------------PARA SELECCIONAR LA PALETA----------------------------//
 
-const previewHeaderStrip = document.querySelector ('.js-card-preview-header');
+const previewHeaderStrip = document.querySelector('.js-card-preview-header');
 //aqui tendrian que ir mas const pero como reutilizo las que ya están declaradas arriba solo pongo esta.
 
-
 //seleciono el atributo comun para poder coger todos los radiobuttons
-const paletteButtons = document.querySelectorAll ('.js-sectionDesign input[name=colourpalette]');
-
+const paletteButtons = document.querySelectorAll(
+  '.js-sectionDesign input[name=colourpalette]'
+);
 
 // funcion que coge el valor del radiobutton seleccionado. Creo ademas una variable para cada color, y en funcion de radiobutton seleccionado escoge un color u otro.
-function handler(){
-
-  const selectedPalette = document.querySelector('.js-sectionDesign input[name=colourpalette]:checked').value;
+function handler() {
+  const selectedPalette = document.querySelector(
+    '.js-sectionDesign input[name=colourpalette]:checked'
+  ).value;
   let colorDark;
   let colorMedium;
   let colorLight;
-  
+
   if (selectedPalette === 'colours1') {
     colorDark = '#114e4e';
     colorMedium = '#438792';
     colorLight = '#a2deaf';
-
-  }else if (selectedPalette === 'colours2'){
+  } else if (selectedPalette === 'colours2') {
     colorDark = '#420101';
     colorMedium = '#bd1010';
     colorLight = '#e95626';
-
-  } else if (selectedPalette === 'colours3'){
+  } else if (selectedPalette === 'colours3') {
     colorDark = '#3e5b65';
     colorMedium = '#eab052';
     colorLight = '#a0c0cf';
   }
- 
+
   previewHeaderStrip.style.borderColor = colorDark;
   previewName.style.color = colorMedium;
   previewJob.style.color = colorLight;
 }
 
-
 //evento de cambio de cada uno de los radiobuttons
-paletteButtons.forEach(radio => {
-  radio.addEventListener ('change', handler)
+paletteButtons.forEach((radio) => {
+  radio.addEventListener('change', handler);
 });
