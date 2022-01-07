2b0ba2d32bb634995c27a9fd53f1c4ffd00e4cce
Begodpo <begodpo@gmail.com>
2022-01-07 14:06:47 +0100

    add localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,6 @@
 const arrowDesign = document.querySelector('.js-arrowdown-design');
 const arrowFill = document.querySelector('.js-arrowdown-fill');
 const arrowShare = document.querySelector('.js-arrowdown-share');
-
 const allLegends = document.querySelectorAll('.js-legend');
 
 for (const eachLegend of allLegends) {
@@ -77,6 +76,22 @@ let data = {
   palette: '01',
 };
 
+// LocalStorage
+
+function setDataInLocalStorage() {
+  localStorage.setItem('dataSaved', JSON.stringify(data));
+}
+
+function getDataFromLocalStorage() {
+  const localStorageData = localStorage.getItem('dataSaved');
+  if (localStorageData !== null) {
+    const object = JSON.parse(localStorageData);
+    data = object;
+    renderInputs();
+    handleFill();
+  }
+}
+
 //Función para pintar el preview con lo que escribes en el input
 function renderInputs() {
   //Mantaner en preview los campos rellenos si no hay nada escrito
@@ -87,6 +102,8 @@ function renderInputs() {
   data.job === ''
     ? (previewJob.innerHTML = 'Front-end developer')
     : (previewJob.innerHTML = data.job);
+
+  setDataInLocalStorage();
 }
 
 //función manejadora guarda valores del input y ejecuta función que pinta
@@ -100,6 +117,7 @@ function handleFill(e) {
 
   //Ejecuto la FUNCIÓN que me pintará en el preview
   renderInputs(data);
+  setDataInLocalStorage(); // Guardar en LocalStorage
 }
 
 //evento de escuchar el formulario entero(Todos los inputs)
@@ -154,6 +172,8 @@ function handler() {
 
   previewHeaderStrip.style.borderColor = colorMedium;
   previewName.style.color = colorDark;
+
+  setDataInLocalStorage();
 }
 
 paletteButtons.forEach((radio) => {
@@ -254,6 +274,8 @@ function handleClickReset() {
 }
 
 buttonReset.addEventListener('click', handleClickReset);
+
+getDataFromLocalStorage();
 
 /*
 const buttonReset = document.querySelector('.js-card_reset');
